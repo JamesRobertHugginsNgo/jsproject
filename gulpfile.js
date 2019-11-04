9d9fb5b5adaa777d983aaa434db0f374a04590e0
James Robert Huggins Ngo <JamesRobert.Ngo@GMail.com>
2019-11-04 00:42:45 -0500

    Add Webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const del = require('del');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
 function cleanup() {
   return del('dist');
@@ -13,4 +15,18 @@ function buildModuleJs() {
     .pipe(gulp.dest('dist'));
 }
 
-exports.default = gulp.series(cleanup, buildModuleJs);
+function bundleWithWebpack() {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stats) => {
+      if (err) {
+        return reject(err)
+      }
+      if (stats.hasErrors()) {
+        return reject(new Error(stats.compilation.errors.join('\n')))
+      }
+      resolve()
+    })
+  })
+}
+
+exports.default = gulp.series(cleanup, gulp.parallel(buildModuleJs, bundleWithWebpack));
